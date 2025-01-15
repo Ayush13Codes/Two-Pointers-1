789688260205b629babb8bb89195de14b7417d1a
Ayush Pawar <ayushpawar@Ayushs-MacBook-Air.local>
2025-01-14 19:13:41 -0500

    Two Ptrs 1 - Problem 1 and 2

---
+++ Problem1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        def swap(p, q):
+            t = nums[p]
+            nums[p] = nums[q]
+            nums[q] = t
+
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1
+            elif nums[i] == 2:
+                swap(r, i)
+                r -= 1
+                i -= 1
+            i += 1
+    # T: O(n), S: O(1)
